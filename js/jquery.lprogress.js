9ca7f4b89570ce5f68afd8ee285378a4c0cce129
M-Ulyanov <web.ulyanov@gmail.com>
2014-10-02 22:42:25 +0400

    Version 0.0.1
    Добавлен числовой индикатор прогресса бара
    Несколько новых параметров:
    - Установка начального значения
    - Максимальное значение
    - Кол-во знаков после запятой

--- js/jquery.lprogress.js
+++ js/jquery.lprogress.js
@@ -1,42 +1,87 @@
 (function($) {
 	jQuery.fn.lprogress = function(options){
+
+		// Параметры по умолчанию
 		var defaults = $.extend({}, {
 		loadReady : false,
-		animateSpeed : 600,
+		startValue: 0,
+		maxValue: 100,
+		animateSpeed : 1000,
+		decimals: 0,
 		}, options);
 		
-		//
-		var globalStep = 0;
+		// Глобальные переменные
+		var globalStep = 0,
+			self = this;
+
+		console.log(defaults)
+
 		
 		return this.each(function() {
 		
 			// Переменные	
 			var progress = $(this),
 				line = $('<div class="inner-progress">'),
-				percentage = $('<span class="percentage ">'),
-				initAnimate = true,
-				buttonstep = $('.progressbar-step');
+				buttonstep = $('.progressbar-step'),
+				numberPercent = $('<span class="number-percent">');
 				
-			//	
+			//	Вставка элементов в Dom
 			$(progress).addClass('progressbar').append(line);
+			$(line).append(numberPercent);
 
-			//
+			// Установка начального значения
+			$(line).css('width', defaults.startValue + '%');
+
+			// Функция автозагрузки
 			if(defaults.loadReady){
 				$(line).animate({
 					width: '100%'
-				}, defaults.animateSpeed)};
+				}, defaults.animateSpeed);
+
+				$(line).find(numberPercent).animate({ 
+					num: defaults.maxValue,
+				}, 
+				{ duration: defaults.animateSpeed,
+					    step: function (num){
+					        this.innerHTML = (num).toFixed(defaults.decimals) + '%'
+	    			}
+				});
+			};
+
+			if(defaults.startValue != 0){
+				
+			}
+
 			
-			//
+			// При клике на элементы управления
 			$('.progressbar-step').on('click', function(event){
-					var dataStep = parseInt($(this).attr('data-step')),
-					dataInit = $(this).attr('data-init'),
-					elem = $('#' + dataInit).find('.inner-progress'),
-					
-					globalStep += dataStep;
-					
-				$(elem).animate({
+				var dataStep = parseInt($(this).attr('data-step')),
+				dataInit = $(this).attr('data-init'),
+				elem = $('#' + dataInit).find('.inner-progress');
+
+				if(defaults.startValue != 0 && globalStep == 0){
+					console.log(defaults.startValue)
+					globalStep = defaults.startValue;
+				}
+				
+				globalStep += dataStep;
+
+				if(globalStep > defaults.maxValue)
+					globalStep = defaults.maxValue;
+				
+				// Анимация полоски и чисел	
+				$(elem).stop(true).animate({
 					width: globalStep + '%',
 				}, defaults.animateSpeed);
+
+				$(elem).find(numberPercent).stop(true).animate({ 
+					num: globalStep
+				}, 
+				{ duration: defaults.animateSpeed,
+					    step: function (num){
+					        this.innerHTML = (num).toFixed(defaults.decimals) + '%'
+	    			}
+				});
 			});
 			
 		});
